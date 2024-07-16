b9e3c1470fcf5bb4b229add2182f6cf68a46d280
bhoon-coding <bhooncoding@gmail.com>
2024-07-16 18:54:56 +0900

    feat: TabView 구현

---
+++ BestEats/TabBarView.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarView.swift
+//  BestEats
+//
+//  Created by BH on 2024/07/16.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }
+            RecommendationView()
+                .tabItem {
+                    Image(systemName: "person.fill.questionmark")
+                    Text("추천")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
