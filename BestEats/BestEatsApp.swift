13a0fa915a53b914cc20d495800d3f4d1758ad42
bhoon-coding <bhooncoding@gmail.com>
2024-07-16 18:54:47 +0900

    chore: 초기세팅 및 asset 추가

--- BestEats/BestEatsApp.swift
+++ BestEats/BestEatsApp.swift
@@ -25,7 +25,7 @@ struct BestEatsApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarView()
         }
         .modelContainer(sharedModelContainer)
     }
