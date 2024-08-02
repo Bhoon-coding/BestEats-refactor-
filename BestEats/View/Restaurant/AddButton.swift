8abaffb216d7076d0d3c79f846859f90013d98fa
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-08-02 18:54:36 +0900

    [Feat] 맛집, 메뉴 추가 버튼 수정 (Float) (#17)
    맛집, 메뉴 추가 버튼 수정

---
+++ BestEats/View/Restaurant/AddButton.swift
@@ -0,0 +1,49 @@
+//
+//  AddRestaurantCard.swift
+//  BestEats
+//
+//  Created by BH on 2024/07/19.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    
+    @State private var isPresented = false
+    
+    var sheet: AddSheetType
+    
+    enum AddSheetType {
+        case restaurant
+        case menu(rateType: Binding<Rate>, restaurant: Restaurant)
+        
+        @ViewBuilder
+        var sheetView: some View {
+            switch self {
+            case .restaurant:
+                AddRestaurantSheet()
+            case .menu(let rateType, let restaurant):
+                AddMenuSheet(rateType: rateType, restaurant: restaurant)
+            }
+        }
+    }
+    
+    var body: some View {
+        Button(action: {
+            self.isPresented = true
+        }, label: {
+            Image(systemName: "plus")
+                .font(.pretendardBold24)
+                .padding()
+                .background(.green)
+                .foregroundStyle(.white)
+                .clipShape(Circle())
+                .shadow(radius: 4, x: 0, y: 4)
+        })
+        .padding()
+        .sheet(
+            isPresented: $isPresented,
+            content: { sheet.sheetView }
+        )
+    }
+}
--- BestEats/View/TabBarView.swift
+++ BestEats/View/TabBarView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct TabBarView: View {
     
+    @State var isLoading: Bool = true
+    
     var body: some View {
-        TabView {
-            RestaurantView()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("홈")
+        ZStack {
+            if isLoading {
+                SplashView()
+                    .transition(.opacity)
+            } else {
+                TabView {
+                    RestaurantView()
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("홈")
+                        }
+                    RecommendationView()
+                        .tabItem {
+                            Image(systemName: "person.fill.questionmark")
+                            Text("추천")
+                        }
+                    SettingsView()
+                        .tabItem {
+                            Image(systemName: "gearshape.fill")
+                            Text("설정")
+                        }
                 }
-            RecommendationView()
-                .tabItem {
-                    Image(systemName: "person.fill.questionmark")
-                    Text("추천")
-                }
-            SettingsView()
-                .tabItem {
-                    Image(systemName: "gearshape.fill")
-                    Text("설정")
-                }
+            }
         }
-        
+        .onAppear {
+            // TODO: [] 추후 데이터 fetch시간이 걸리는경우 로딩 구현
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                withAnimation {
+                    isLoading = false
+                }
+            }
+        }
     }
 }
 
