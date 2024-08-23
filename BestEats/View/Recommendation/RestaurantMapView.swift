3d0fccb558ebce273ff2d80c67eeb17bb3dfeffb
bhoon-coding <bhooncoding@gmail.com>
2024-08-23 15:24:34 +0900

    refactor: view 프로퍼티 처리

--- BestEats/View/Recommendation/RestaurantMapView.swift
+++ BestEats/View/Recommendation/RestaurantMapView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 import MapKit
 
+enum PlaceInfoError: Error {
+    case emptyData
+    
+    var messageDescription: String {
+        switch self {
+        case .emptyData:
+            return "정보없음"
+        }
+    }
+}
+
 struct RestaurantMapView: View {
     
     @Environment(\.dismiss) private var dismiss
@@ -17,145 +28,14 @@ struct RestaurantMapView: View {
     @State private var selectedPlace: V2.Local.Search.Keyword.Response.PlaceInfo?
     @State var foodType: FoodType = .cafe
     
-    
-    private var map: some View {
-        Map(
-            coordinateRegion: $vm.region,
-            showsUserLocation: true,
-            //                userTrackingMode: .constant(.follow),
-            annotationItems: vm.nearRestaurants
-        ) { nearPlace in
-            MapAnnotation(coordinate: nearPlace.coordinate) {
-                CustomMapMarkerView(
-                    selectedPlace: $selectedPlace,
-                    place: nearPlace,
-                    type: foodType
-                )
-            }
-        }
-        .ignoresSafeArea(edges: .top)
-    }
-    
-    private var searchedPlaceText: some View {
-//        Text("근처에 n개의 맛집이 있어요!")
-        Text(setInfoNearPlaceCount())
-            .font(.pretendardBold14)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.black.opacity(0.5))
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-    }
-    
-    private var menuButton: some View {
-        Button(action: {
-            self.isPresentedSheet = true
-        }, label: {
-            Image(systemName: "line.3.horizontal")
-                .font(.pretendardBold18)
-        })
-        .frame(width: 48, height: 48)
-        .foregroundStyle(.white)
-        .background(.green)
-        .clipShape(Circle())
-    }
-    
-    private var currentLocationButton: some View {
-        Button(action: {
-            vm.getCurrentLocation()
-        }, label: {
-            Image(Img.currentLocation)
-                .resizable()
-                .frame(width: 24, height: 24)
-        })
-        .padding()
-        .background(.green)
-        .clipShape(Circle())
-    }
-    
     var body: some View {
         ZStack {
             map
             
             VStack {
-                ZStack {
-                    searchedPlaceText
-                    
-                    HStack {
-                        Spacer()
-                        
-                        menuButton
-                    }
-                }
-                
-                
+                topContentContainer
                 Spacer()
-                
-                VStack {
-                    HStack {
-                        Spacer()
-                        currentLocationButton
-                    }
-                    .padding(.bottom, 16)
-                    
-                    HStack(spacing: 16) {
-                        VStack(alignment: .leading) {
-                            Text(setPlaceName())
-                                .font(.pretendardBold20)
-                                .padding(.bottom, 24)
-                            
-                            HStack(alignment: .bottom) {
-                                VStack(alignment: .leading, spacing: 8) {
-                                    Text(setCategoryName())
-                                    Text(setAddressName())
-                                }
-                                .font(.pretendardRegular14)
-                                .foregroundStyle(.gray)
-                                
-                                Spacer()
-                                
-                                Button {
-                                    print("길안내는 준비중 입니다")
-                                } label: {
-                                    HStack {
-                                        Image(systemName: "location.fill")
-                                        Text(setDistance())
-                                    }
-                                }
-                                .font(.pretendardBold14)
-                                .padding(8)
-                                .foregroundStyle(.white)
-                                .background(.green)
-                                .clipShape(RoundedRectangle(cornerRadius: 8))
-                            }
-                        }
-                        .padding()
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
-                        
-                        VStack {
-                            Button("더보기") {
-                                // TODO: [] WebView 띄우기
-                            }
-                            .padding()
-                            .frame(width: 88)
-                            .background(.green)
-                            .clipShape(RoundedRectangle(cornerRadius: 16))
-                            
-                            Spacer()
-                            
-                            Button("예약하기") {
-                                // TODO: [] 전화 팝업 띄우기
-                            }
-                            .padding()
-                            .frame(width: 88)
-                            .background(.red)
-                            .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        .font(.pretendardBold14)
-                        .foregroundStyle(.white)
-                    }
-                    .frame(maxHeight: 128)
-                }
+                bottomContentContainer
             }
             .padding(.horizontal)
             .padding(.vertical, 24)
@@ -182,17 +62,6 @@ struct RestaurantMapView: View {
             FoodTypeSheetView(foodType: $foodType)
                 .presentationDetents([.fraction(0.5), .large])
                 .presentationDragIndicator(.visible)
-        }
-    }
-    
-    enum PlaceInfoError: Error {
-        case emptyData
-        
-        var messageDescription: String {
-            switch self {
-            case .emptyData:
-                return "정보없음"
-            }
         }
     }
     
@@ -248,6 +117,163 @@ struct RestaurantMapView: View {
         withAnimation(.easeIn) {
             vm.region.center = location
         }
+    }
+}
+
+// MARK: - View
+
+extension RestaurantMapView {
+    
+    private var map: some View {
+        Map(
+            coordinateRegion: $vm.region,
+            showsUserLocation: true,
+            //                userTrackingMode: .constant(.follow),
+            annotationItems: vm.nearRestaurants
+        ) { nearPlace in
+            MapAnnotation(coordinate: nearPlace.coordinate) {
+                CustomMapMarkerView(
+                    selectedPlace: $selectedPlace,
+                    place: nearPlace,
+                    type: foodType
+                )
+            }
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+    
+    private var topContentContainer: some View {
+        ZStack {
+            searchedPlaceText // 가운데로 두기 위함
+            
+            HStack {
+                Spacer()
+                menuButton
+            }
+        }
+    }
+    
+    private var searchedPlaceText: some View {
+        Text(setInfoNearPlaceCount())
+            .font(.pretendardBold14)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.black.opacity(0.5))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+    
+    private var menuButton: some View {
+        Button(action: {
+            self.isPresentedSheet = true
+        }, label: {
+            Image(systemName: "line.3.horizontal")
+                .font(.pretendardBold18)
+        })
+        .frame(width: 48, height: 48)
+        .foregroundStyle(.white)
+        .background(.green)
+        .clipShape(Circle())
+    }
+    
+    private var bottomContentContainer: some View {
+        VStack(spacing: 16) {
+            currentLocationContainer
+            placeInfoAndButtonContainer
+        }
+    }
+    
+    private var currentLocationContainer: some View {
+        HStack {
+            Spacer()
+            currentLocationButton
+        }
+    }
+    
+    private var currentLocationButton: some View {
+        Button(action: {
+            vm.getCurrentLocation()
+        }, label: {
+            Image(Img.currentLocation)
+                .resizable()
+                .frame(width: 24, height: 24)
+        })
+        .padding()
+        .background(.green)
+        .clipShape(Circle())
+    }
+    
+    private var placeInfoAndButtonContainer: some View {
+        HStack(spacing: 16) {
+            placeInfoContainer
+            placeInfoButtonContainer
+        }
+        .frame(maxHeight: 128)
+    }
+    
+    private var placeInfoContainer: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text(setPlaceName())
+                .font(.pretendardBold20)
+            
+            HStack(alignment: .bottom) {
+                placeInfoContents
+                Spacer()
+                placeDistanceButton
+            }
+        }
+        .padding()
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+    
+    private var placeInfoContents: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(setCategoryName())
+            Text(setAddressName())
+        }
+        .font(.pretendardRegular14)
+        .foregroundStyle(.gray)
+    }
+    
+    private var placeDistanceButton: some View {
+        Button {
+            // TODO: [] 길안내 준비중 팝업
+            print("길안내는 준비중 입니다")
+        } label: {
+            HStack {
+                Image(systemName: "location.fill")
+                Text(setDistance())
+            }
+        }
+        .font(.pretendardBold14)
+        .padding(8)
+        .foregroundStyle(.white)
+        .background(.green)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+    
+    private var placeInfoButtonContainer: some View {
+        VStack {
+            Button("더보기") {
+                // TODO: [] WebView 띄우기
+            }
+            .padding()
+            .frame(width: 88)
+            .background(.green)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            
+            Spacer()
+            
+            Button("예약하기") {
+                // TODO: [] 전화 팝업 띄우기
+            }
+            .padding()
+            .frame(width: 88)
+            .background(.red)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .font(.pretendardBold14)
+        .foregroundStyle(.white)
     }
 }
 
